0021951ddc4d8fa288b06b6632524e9e77205cca
Lalomax <lalomax365@gmail.com>
2024-07-31 16:04:38 -0400

    feat: 4.5 Auxiliar function and unit test

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -10,7 +10,15 @@ const totalLikes = (blogs) => {
   return blogs.length === 0 ? 0 : blogs.reduce(reducer, 0);
 };
 
+const favoriteBlog = (blogs) => { 
+  if (blogs.length === 0) {
+    return []
+  }
+  return blogs.reduce((a, b) => (a.likes > b.likes ? a : b))
+}
+
 module.exports = {
   // dummy,
+  favoriteBlog,
   totalLikes,
 };
